39dcdaedbb3b8ece1f192269ac2b9ffaf945ac30
Muthukarthick T <98314834+Muka36@users.noreply.github.com>
2022-04-15 12:24:03 +0530

    Add files via upload

---
+++ problem.js
@@ -0,0 +1,33 @@
+// import React, { useState } from 'react'
+
+
+// const Grandchild = props => {
+//   return(
+//     <div>
+//       <h1>Grandchild:</h1>
+//       <Child brand={props.brand}/>
+//     </div>
+//   )
+// }
+
+
+// const Child = props => {
+//   return (
+//     <div>
+//       <h1>From Karthick{props.brand}</h1>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const[brandname] = useState("6000");
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+       
+//       <Grandchild brand ={brandname}/>
+//     </div>
+//   )
+// }
+
+// export default App;
